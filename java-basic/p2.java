f11f3fb46d0d8b45c611e5065826da9dd467084b
mean <meanstack4@gmail.com>
2024-10-18 12:26:22 +0530

    First program update

---
+++ java-basic/p2.java
@@ -0,0 +1,7 @@
+class Prog2{
+    public static void main(String lili[]){
+       System.out.println("GM.....");
+       System.out.println("GN.....");
+       System.out.println("Hello.......");
+    }
+}
